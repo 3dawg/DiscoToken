5f5877c07b3a4a1305f9bbba24d29858cededa1b
Chaotic <50114982+3dawg@users.noreply.github.com>
2021-09-09 16:32:00 -0500

    uploaded new changes that gave it an http proxy

--- chkr.py
+++ chkr.py
@@ -1,6 +1,16 @@
 import requests
 import time
 from colorama import Fore, init
+import urllib
+import http.server
+import socketserver
+
+PORT = 9067
+
+#here we define the class to actually use the proxy
+#Todo: Add SSL Functionality
+
+
 
 print(
     f"""{Fore.GREEN}
@@ -22,13 +32,32 @@ Note: You should {Fore.RED}NEVER{Fore.GREEN} do more than 50 tokens at once beca
 Ps: This tool is made for educational purposes only I am not liable for any actions you do while using this
 """
 )
+
+
 tknname = input("Drag and drop your tokenlist: ")
+
+#i added it here for now 
+#here we define the class to actually use the proxy
+#Todo: Add SSL Functionality
+
+class MyProxy(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        url=self.path[1: ]
+        self.send_response(200)
+        self.end_headers()
+        self.copyfile(urllib.urlopen(url), self.wfile)
+httpd = socketserver.TCPServer(('', PORT), MyProxy)
+print ("Now serving at " + str(PORT))
+httpd.serve_forever()
+proxies = {
+  'http': 'localhost:9067'
+}
 with open(f"{tknname}", "r") as f:
     for line in f:
         time.sleep(1)
         token = line.rstrip("\n")
         headers = {"Authorization": f"{token}"}
-        src = requests.get("https://discordapp.com/api/v6/auth/login", headers=headers)
+        src = requests.get("http://discordapp.com/api/v6/auth/login", headers=headers, proxies=proxies)
 
         try:
             if src.status_code == 200:
@@ -38,3 +67,4 @@ with open(f"{tknname}", "r") as f:
         except Exception:
             print("Something broke lmfao contact discord suppport or open an issue :)")
 print("Finished checking!")
+
